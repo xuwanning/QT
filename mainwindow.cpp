80b06390e70a6a08e197e870a0432bd4e85f87bd
XXRRSSRR <2960747801@qq.com>
2023-11-07 23:11:04 +0800

    finsh main ui design

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("length:   "+QString::number(0)+"lines:    "+QString::number(1));
+    ui->statusBar->addPermanentWidget(&statusLabel);
+
+    statusCursorlabel.setMaximumWidth(150);
+    statusCursorlabel.setText("Ln: "+QString::number(0)+"Col: "+QString::number(1));
+    ui->statusBar->addPermanentWidget(&statusLabel);
+
+    QLabel *author = new QLabel(ui->statusBar);
+    author->setText(tr("孙家兴"));
+    ui->statusBar->addPermanentWidget(author);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    QLabel statusCursorlabel;
+    QLabel statusLabel;
+};
+
+#endif // MAINWINDOW_H
