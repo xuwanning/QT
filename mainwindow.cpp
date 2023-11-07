ee47714e6643a8840e7e0880689b806546052867
XXRRSSRR <2960747801@qq.com>
2023-11-07 23:30:42 +0800

    finish ui design

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,7 +5,8 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);  //主界面设计完成
+
 
 
     statusLabel.setMaximumWidth(150);
